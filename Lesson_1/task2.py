8bdde2f227d2b9197063b15c3c46ffb081ee8743
dimkaso <110856738+dimkaso@users.noreply.github.com>
2022-10-18 20:22:03 +0300

    Create task2.py

---
+++ Lesson_1/task2.py
@@ -0,0 +1,26 @@
+import math
+while True:
+    value_a = float(input("Enter A: "))
+    value_b = float(input("Enter B: "))
+    operation = str(input("Choose operation(+,-,*,/,^,sqrt): "))
+    if operation=="+":
+        print(f'A+B {value_a+value_b}')
+    
+    if operation=="-":
+        print(f'A-B {value_a-value_b}')
+    
+    if operation=="*":
+        print(f'A*B {value_a*value_b}')
+    
+    if operation=="/":
+        print(f'A/B {value_a/value_b}')
+    
+    if operation=="^":
+        print(f'A^2 {value_a**2}' )
+        print(f'B^2 {value_b**2}' )
+        
+    if operation=="sqrt":
+        print(f'sqrt A {int(math.sqrt(value_a))}' )
+        print(f'sqrt B {int(math.sqrt(value_b))}' )
+    else:
+        print("Nein mein Führer")
